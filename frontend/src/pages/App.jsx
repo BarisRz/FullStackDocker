fd7cb06594cb15a58059772ad109001a4adfc4c0
Baris G <barisg@pm.me>
2025-01-06 16:29:01 +0100

    first commit

---
+++ frontend/src/pages/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="flex w-screen h-screen items-center justify-center bg-black text-white">
+        Template Fullstack avec déploiement docker facile, oubliez pas de
+        configurer le .env
+      </div>
+    </>
+  );
+}
+
+export default App;
