8e6b7c4ec1a8b0eedd0f9197fbd9399deaffbb2f
BarisRz <barisg@pm.me>
2026-08-12 18:42:27 +0200

    remplace nodemailer par resend

--- backend/src/middlewares/mailling.js
+++ backend/src/middlewares/mailling.js
@@ -1,55 +1,47 @@
 require("dotenv").config({ path: "../../.env" });
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 const crypto = require("crypto");
 const { userManager } = require("../managers");
 const generateHtmlContent = require("./generateHtmlContent");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 const generateToken = () => {
   return crypto.randomBytes(32).toString("hex");
 };
 
 const sendMail = async (req, res, next) => {
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: req.body.email,
-    subject: `Vérifier votre adresse e-mail - Lorga`,
-    html: generateHtmlContent(
-      "emailConfirmation",
-      req.body.pseudo,
-      req.body.token,
-    ),
-  };
   try {
-    await transporter.sendMail(mailOptions);
+    await resend.emails.send({
+      from: "onboarding@resend.dev", // à remplacer par ton domaine vérifié plus tard
+      to: req.body.email,
+      subject: `Vérifier votre adresse e-mail - Lorga`,
+      html: generateHtmlContent(
+        "emailConfirmation",
+        req.body.pseudo,
+        req.body.token,
+      ),
+    });
     console.log("Email sent to:", req.body.email);
     next();
   } catch (error) {
-    console.error("Nodemailer error:", error);
-    res.status(500).json({ Nodemailer: error });
+    console.error("Resend error:", error);
+    res.status(500).json({ Resend: error });
   }
 };
 
 const sendMailPasswordReset = async (req, res, next) => {
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: req.body.email,
-    subject: `Réinitialiser son mot de passe - Lorga`,
-    html: generateHtmlContent("password", "none", req.body.token),
-  };
   try {
-    await transporter.sendMail(mailOptions);
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: req.body.email,
+      subject: `Réinitialiser son mot de passe - Lorga`,
+      html: generateHtmlContent("password", "none", req.body.token),
+    });
     console.log("Email for password reset sent to:", req.body.email);
     next();
   } catch (error) {
+    console.error("Resend error:", error);
     res
       .status(500)
       .json({ error: "An error occurred while sending the email." });
