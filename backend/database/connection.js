afeba26ca98d09271b1755108a3d6075cecb3cd6
BarisRz <barisg@pm.me>
2026-08-12 15:37:04 +0200

    debugging connection in railway

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 
 const mysql = require("mysql2/promise");
 
-// Dont forget to setup your own .env
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+
 const database = mysql.createPool({
-  host: process.env.DB_HOST, // address of the server
-  port: process.env.DB_PORT, // port of the DB server (mysql), not to be confused with the PORT !
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
